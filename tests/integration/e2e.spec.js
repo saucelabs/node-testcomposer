967fdcd9b4d98d31156e9aa8c9babd3e899555a7
Tian Feng <tian.feng@saucelabs.com>
2023-08-21 13:18:03 -0700

    add tests

--- tests/integration/e2e.spec.js
+++ tests/integration/e2e.spec.js
@@ -16,9 +16,9 @@ beforeAll(() => {
   })
 });
 
-test('creating reports', async () => {
+test('creating JavaScript framework reports', async () => {
   const job = await client.createReport({
-    name: "node-testcomposer - report creation",
+    name: "node-testcomposer - JS report creation",
     passed: true,
     startTime: new Date().toISOString(),
     endTime: new Date().toISOString(),
@@ -39,5 +39,119 @@ test('creating reports', async () => {
   s.push(null);
 
   const uploads = await client.uploadAssets(job.id, [{filename: "console.log", data: s}]);
+  expect(uploads.uploaded.length).toBe(1);
+});
+
+test('creating selenium reports', async () => {
+  const job = await client.createReport({
+    name: "node-testcomposer - selenium report creation",
+    passed: true,
+    startTime: new Date().toISOString(),
+    endTime: new Date().toISOString(),
+    browserName: "Chrome",
+    browserVersion: "105",
+    framework: "webdriver",
+    platformName: "Windows 11"
+  });
+
+  expect(job.id).toBeDefined();
+  expect(job.url).toBeDefined();
+
+  console.log(`Job creation successful: ${job.url}`);
+
+  const content = `
+  {
+    "screenshot": null,
+    "suggestion_values": [
+ 
+    ],
+    "request": {
+
+    },
+    "result": {
+
+    },
+    "path": "element",
+    "HTTPStatus": 200,
+    "method": "POST",
+    "statusCode": 0 // 0 for passed status, non-zero for failed
+  }
+]`;
+
+  const s = new Readable();
+  s.push(content);
+  s.push(null);
+
+  const uploads = await client.uploadAssets(job.id, [{filename: "log.json", data: s}]);
+  expect(uploads.uploaded.length).toBe(1);
+});
+
+test('creating Espresso reports', async () => {
+  const job = await client.createReport({
+    name: "node-testcomposer - espresso report creation",
+    passed: true,
+    startTime: new Date().toISOString(),
+    endTime: new Date().toISOString(),
+    framework: "espresso",
+    platformName: "Linux",
+    deviceName: "Android GoogleAPI Emulator",
+    platformVersion: "12.0.",
+  });
+
+  expect(job.id).toBeDefined();
+  expect(job.url).toBeDefined();
+
+  console.log(`Job creation successful: ${job.url}`);
+
+  const content = `{
+    "package_name": "com.saucelabs.mydemoapp.android",
+    "status": "success",
+    "stdout": "I am output",
+    "tests": [
+      {
+        "class": "com.saucelabs.mydemoapp.android.view.activities.DashboardToCheckout",
+        "name": "dashboardProductTest",
+        "status": "success",
+        "status_code": 0,
+        "test_number": "1"
+      }
+    ]
+  }`;
+  const s = new Readable();
+  s.push(content);
+  s.push(null);
+
+  const uploads = await client.uploadAssets(job.id, [{filename: "native-log.json", data: s}]);
+  expect(uploads.uploaded.length).toBe(1);
+});
+
+test('creating XCUITest reports', async () => {
+  const job = await client.createReport({
+    name: "node-testcomposer - xcuitest report creation",
+    passed: true,
+    startTime: new Date().toISOString(),
+    endTime: new Date().toISOString(),
+    framework: "xcuitest",
+    platformName: "Mac 12",
+    deviceName: "iPhone 11 Simulator",
+    platformVersion: "16.2",
+  });
+
+  expect(job.id).toBeDefined();
+  expect(job.url).toBeDefined();
+
+  console.log(`Job creation successful: ${job.url}`);
+
+  const content = `<testsuites name="All tests" tests="1" failures="0">
+  <testsuite name="DemoAppUITests.DemoAppUIAnotherTests" tests="1" failures="0">
+      <testcase classname="DemoAppUITests.DemoAppUIAnotherTests" name="testExample" time="9.561" />
+  </testsuite>
+</testsuites>`;
+
+  const s = new Readable();
+  s.push(content);
+  s.push(null);
+
+  const uploads = await client.uploadAssets(job.id, [{filename: "junit.xml", data: s}]);
   expect(uploads.uploaded.length).toBe(1);
 });
