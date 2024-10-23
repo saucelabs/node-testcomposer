86fe19e554f6148bcd4c95c7628e8062a8c840f5
Mike Han <56001373+mhan83@users.noreply.github.com>
2024-10-23 15:25:52 -0600

    chore: Update tooling config (#31)
    * deps: Remove unused babel dependencies
    
    * chore: update jest config
    
    * deps: remove @typescript/* deps as they are included in typescript-eslint
    
    * deps: update eslint deps
    
    * chore: update eslint config
    
    * deps: Make prettier an exact dep
    
    Even patch changes can have different formatting rules so we don't want
    folks to be applying different rules and having changes go back and
    forth between PRs.
    
    * style: Apply basic prettier formatting
    
    * chore: Remove deprecated eslint rule
    
    * chore: Add ts-check header to eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+// @ts-check
+import ts from "typescript-eslint";
+import js from "@eslint/js";
+import prettier from "eslint-config-prettier";
+import jest from "eslint-plugin-jest";
+
+export default ts.config(
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  prettier,
+  {
+    ignores: ["lib/**"],
+  },
+  {
+    files: ["**/*.*js", "**/*.*ts"],
+    rules: {
+      "@typescript-eslint/no-require-imports": "warn",
+    },
+  },
+  {
+    files: ["tests/**/*.*js", "tests/**/*.*ts"],
+    ...jest.configs["flat/recommended"],
+  },
+  {
+    languageOptions: {
+      globals: {
+        __dirname: true,
+        console: true,
+        exports: true,
+        module: true,
+        require: true,
+        process: true,
+      },
+    },
+  },
+);
--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,9 @@
+/** @type {import('jest').Config} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  clearMocks: true,
+  coverageProvider: "v8",
+  testEnvironment: "node",
   transform: {
-    '^.+\\.ts?$': 'ts-jest',
+    "^.+\\.ts?$": "ts-jest",
   },
-  transformIgnorePatterns: ['<rootDir>/node_modules/'],
 };
